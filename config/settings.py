ad2190b1a7aacc9a2449eec0438511c8282c11a7
Jo-Chang <74034844+Jo-Chang@users.noreply.github.com>
2023-05-18 01:27:26 +0900

    Add dotenv module

--- config/settings.py
+++ config/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!c49%l^)%m@nxdk+qktw@rxeo9exk3o)_5@=(%kgyon17)(ias'
+# SECRET_KEY = 'django-insecure-!c49%l^)%m@nxdk+qktw@rxeo9exk3o)_5@=(%kgyon17)(ias'
+load_dotenv()
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
