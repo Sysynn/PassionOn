f7498b1e57ee3df884f90acc768aa32b2dd54831
Jo-Chang <74034844+Jo-Chang@users.noreply.github.com>
2023-05-18 01:30:50 +0900

    Add allowed_host

--- config/settings.py
+++ config/settings.py
@@ -29,7 +29,9 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 
+                 'port-0-cloudtype-test-1maxx2algn9766g.sel3.cloudtype.app',
+                 ]
 
 
 # Application definition
